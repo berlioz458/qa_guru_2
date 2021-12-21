d001c202bf4235c38a35ef7e0ed0aab439761891
berlioz458 <berlioz458@gmail.com>
2021-12-21 22:00:34 +0700

    clean code

--- src/test/java/com/demoqa/StudentRegistrationFormTest.java
+++ src/test/java/com/demoqa/StudentRegistrationFormTest.java
@@ -1,7 +1,6 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
