0ed2f04e0a31849958b525dff39d9c5103bee353
Shulinina Kate <shulinina.k@yandex.ru>
2022-01-25 17:07:57 +0700

    add allure listener

--- src/test/java/com/demoqa/StudentRegistrationFormTest.java
+++ src/test/java/com/demoqa/StudentRegistrationFormTest.java
@@ -1,6 +1,8 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -21,6 +23,7 @@ public class StudentRegistrationFormTest {
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://demoqa.com/automation-practice-form");
     }
 
