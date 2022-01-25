fb9807d59f42dead74d3f65eb6c38c0ebcbbf8b5
Shulinina Kate <shulinina.k@yandex.ru>
2022-01-25 16:42:48 +0700

    add simple test

---
+++ src/test/java/com/demoqa/SimpleTests.java
@@ -0,0 +1,13 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTests {
+
+    @Test
+    void test01() {
+        assertTrue(true);
+    }
+}
